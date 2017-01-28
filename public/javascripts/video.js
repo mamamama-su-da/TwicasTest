364100cef0b112f510727a8dd1bc44e3763c1a95
junya.masuda <ju.junya.masuda@gmail.com>
2017-01-28 11:30:14 +0900

    ボタンを押すとvideoをキャプチャできるようになった（エラーでるけど）

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -1,3 +1,21 @@
-/**
- * Created by junya.masuda on 2017/01/28.
- */
+$(function() {
+  $('#capture-button').click(getSnap);
+
+  var video = $('#video').get(0);
+  var canvas = $('#tmp-canvas').get(0);
+  console.log(canvas);
+  var ctx = canvas.getContext('2d');
+
+  function getSnap(){
+    ctx.drawImage(video,0,0);
+    var img = new Image();
+    img.src = canvas.toDataURL('image/jpeg');
+    img.onload = function(){
+      img.width = img.width / 2;
+      img.height = img.height / 2;
+      console.log(img.width);
+      $('#capture').append(img);
+    }
+  }
+});
+
