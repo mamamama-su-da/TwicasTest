efff168e84923df022b342658af71e22eab55685
mikesorae <mikesorae>
2017-01-28 15:20:55 +0900

    検証用パラメータ

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  clientId: 'hogehoge',
-  clientSecret: 'hogehoge',
-  redirectUrl: 'http://localhost:3000/login/callback',
+  clientId: '170960297.b66f4f5e1c41ce71d2c989039da114e9cc222263939a9c2d313234b592eb9ef6',
+  clientSecret: '862f9bf0f1077f81dd87d8bbd7335f73ab95ca7fd31014dc423ac3e121c7ee9e',
+  redirectUrl: 'http://52.199.252.67/login/callback'
 };
