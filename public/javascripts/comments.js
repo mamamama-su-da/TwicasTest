c789646bae95ed1aa429122f76824aafc4f3f721
mikesorae <mikesorae>
2017-01-28 18:11:27 +0900

    ユーザ情報取得とかの整理

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -5,16 +5,16 @@ console.log(token);
 
 var commentApiUrl = '/comments';
 
-$.ajax({
-  url: commentApiUrl,
-  type: 'GET',
-  data: {
-  }
-})
-.done(function(res) {
-  console.log(res);
-})
-.fail(function(err, reason) {
-  console.log('failed');
-  console.log(reason);
-});
+// $.ajax({
+//   url: commentApiUrl,
+//   type: 'GET',
+//   data: {
+//   }
+// })
+// .done(function(res) {
+//   console.log(res);
+// })
+// .fail(function(err, reason) {
+//   console.log('failed');
+//   console.log(reason);
+// });
